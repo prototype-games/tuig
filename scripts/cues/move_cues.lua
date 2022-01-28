f04a9439ab4e6a7a7d68a55db912c5d743a9b1a1
Simon Struck <simon@simonscode.org>
2022-01-28 22:19:02 +0100

    Remove here prints

--- scripts/cues/move_cues.lua
+++ scripts/cues/move_cues.lua
@@ -1,23 +1,21 @@
 local frame = {}
 frame["move_relative_cue"] = function(scene, actor, x, y)
-		local lines = {{name="slide_move_fixed_speed", x=actor.x+x, y=actor.y+y, speed= 400}
-		 }
+		local lines = {
+			{name="slide_move_fixed_speed", x=actor.x+x, y=actor.y+y, speed= 400}
+		}
 
 		lines_loader.push_to_lines(scene.lines[actor], "move_relative_cue", lines,1)
-		print("HERE")
 	end
 
 
 
 frame["move_to_coords_cue"] = 
 	function(scene, actor, x, y)
-	
-		local lines = { {name="slide_move_fixed_speed", x=x, y=y, speed= 400}}
+		local lines = {
+			{name="slide_move_fixed_speed", x=x, y=y, speed= 400}
+	}
 
-	 	lines_loader.push_to_lines(scene.lines[actor], "move_to_coords_cue", lines)
-	 	print("HERE")
+		lines_loader.push_to_lines(scene.lines[actor], "move_to_coords_cue", lines)
 	end
-
-
 
 return frame
