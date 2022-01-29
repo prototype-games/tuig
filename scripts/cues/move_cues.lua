05196b9d4d8fb8efc2b7638687c529315b24e9c8
Nander Voortman <nanderv@gmail.com>
2022-01-29 17:04:21 +0100

    Cleanup of move cues file

--- scripts/cues/move_cues.lua
+++ scripts/cues/move_cues.lua
@@ -1,21 +1,19 @@
 local frame = {}
-frame["move_relative_cue"] = function(scene, actor, x, y)
-		local lines = {
-			{name="slide_move_fixed_speed", x=actor.x+x, y=actor.y+y, speed= 400}
-		}
-
-		lines_loader.push_to_lines(scene.lines[actor], "move_relative_cue", lines,1)
-	end
-
-
-
-frame["move_to_coords_cue"] = 
-	function(scene, actor, x, y)
-		local lines = {
-			{name="slide_move_fixed_speed", x=x, y=y, speed= 400}
+function frame.move_relative_cue (scene, actor, x, y)
+	local lines = {
+		{name="slide_move_fixed_speed", x=actor.x+x, y=actor.y+y, speed= 400}
 	}
 
- 	lines_loader.add_to_lineset_or_push(scene.lines[actor], "move_to_coords_cue", lines, {"move_to_coords_cue"})
+	lines_loader.push_to_lines(scene.lines[actor], "move_relative_cue", lines,1)
+end
+
+
+function frame.move_to_coords_cue(scene, actor, x, y)
+	local lines = {
+		{name="slide_move_fixed_speed", x=x, y=y, speed= 400}
+	}
+
+	lines_loader.add_to_lineset_or_push(scene.lines[actor], "move_to_coords_cue", lines, {"move_to_coords_cue"})
 end
 
 return frame
