fa68757ba722927124b8bcef031d5ed0b7d6c440
Nander Voortman <nanderv@gmail.com>
2022-03-29 12:19:59 +0200

    Remove commented out code

--- lib/tuig/directors.lua
+++ lib/tuig/directors.lua
@@ -9,15 +9,4 @@ function lib.init()
 	return res
 end
 
--- function lib:all_directors(obj, prefix)
--- 	for k,v in pairs(obj) do
--- 		if type(v) == "table" then
--- 			if v.draw or v.update or v.init then
--- 				v.name = prefix..k
--- 			else
--- 				get_lines(v, prefix..k..".")
--- 			end
--- 		end
--- 	end
--- end
 return lib
